513342dd464ee98d99bc4220e9c8a5081f8995de
osip-000 <osipoff96d@gmail.com>
2018-01-16 12:26:41 +0700

    Added toolbar for Gallery

--- TestTask/View/ViewControllers/Gallery/GalleryViewController.swift
+++ TestTask/View/ViewControllers/Gallery/GalleryViewController.swift
@@ -5,10 +5,18 @@ import ImageSlideshow
 class GalleryViewController: UIViewController {
 
     @IBOutlet weak var imageSlideshow: ImageSlideshow!
+    @IBOutlet weak var btnLeft: UIBarButtonItem!
+    @IBOutlet weak var btnRight: UIBarButtonItem!
+    
+    private var currentPage = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initImageSladeshow()
+    }
+    
+    private func initImageSladeshow() {
         imageSlideshow.backgroundColor = UIColor.white
         imageSlideshow.pageControlPosition = PageControlPosition.underScrollView
         imageSlideshow.pageControl.currentPageIndicatorTintColor = UIColor.lightGray
@@ -18,8 +26,27 @@ class GalleryViewController: UIViewController {
         imageSlideshow.zoomEnabled = true
         imageSlideshow.circular = false
         imageSlideshow.preload = ImagePreload.fixed(offset: 1)
-
+        
         imageSlideshow.setImageInputs(GalleryImages.images as! [InputSource])
+        
+        imageSlideshow.currentPageChanged = { page in
+            self.currentPage = page
+        }
     }
     
+    @IBAction func ClickLeftBtn(_ sender: Any) {
+        let newPage = currentPage - 1
+        if newPage >= 0 {
+            currentPage = newPage
+            imageSlideshow.setCurrentPage(currentPage, animated: true)
+        }
+    }
+    
+    @IBAction func ClickRightBtn(_ sender: Any) {
+        let newPage = currentPage + 1
+        if newPage < imageSlideshow.images.count {
+            currentPage = newPage
+            imageSlideshow.setCurrentPage(currentPage, animated: true)
+        }
+    }
 }

f43269bf1c8e7757a03eac8f4532f8bd22fd23d3
osip-000 <osipoff96d@gmail.com>
2018-01-16 13:32:14 +0700

    Added disable btns

--- TestTask/View/ViewControllers/Gallery/GalleryViewController.swift
+++ TestTask/View/ViewControllers/Gallery/GalleryViewController.swift
@@ -13,6 +13,7 @@ class GalleryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        btnLeft.isEnabled = false
         initImageSladeshow()
     }
     
@@ -30,7 +31,21 @@ class GalleryViewController: UIViewController {
         imageSlideshow.setImageInputs(GalleryImages.images as! [InputSource])
         
         imageSlideshow.currentPageChanged = { page in
+            self.enabledBtnsToolbar(page: page)
             self.currentPage = page
+        }
+    }
+    
+    private func enabledBtnsToolbar(page: Int) {
+        self.btnRight.isEnabled = true
+        self.btnLeft.isEnabled = true
+        
+        if page == 0 {
+            self.btnLeft.isEnabled = false
+        } else if self.currentPage <= page {
+            if page >= self.imageSlideshow.images.count - 1 {
+                self.btnRight.isEnabled = false
+            }
         }
     }
     
